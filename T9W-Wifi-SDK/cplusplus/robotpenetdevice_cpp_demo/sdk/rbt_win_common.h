2d7fdcf34a722811c4c015c2266a54a10dfe303a
lee <zlp3817@gmail.com>
2018-08-25 19:12:19 +0800

    1.1.1.9 修改初始化结构体

--- T9W-Wifi-SDK/cplusplus/robotpenetdevice_cpp_demo/sdk/rbt_win_common.h
+++ T9W-Wifi-SDK/cplusplus/robotpenetdevice_cpp_demo/sdk/rbt_win_common.h
@@ -26,7 +26,7 @@ enum keyPressEnum
 };
 
 //设备上线回调
-typedef void __stdcall onAccept(rbt_win_context* context, const char* pClientIpAddress );
+typedef void __stdcall onAccept(rbt_win_context* context, const char* pClientIpAddress);
 //错误包回调
 typedef void __stdcall onErrorPacket(rbt_win_context* context);
 //笔迹数据回调
@@ -36,7 +36,7 @@ typedef void __stdcall onDeviceMac(rbt_win_context* context, const char* pMac);
 //设备名称回调
 typedef void __stdcall onDeviceName(rbt_win_context* context, const char* pMac, const char* pName);
 //设置设备名称回调
-typedef void __stdcall onDeviceNameResult(rbt_win_context* context, const char* pMac,int res,const char* pName);
+typedef void __stdcall onDeviceNameResult(rbt_win_context* context, const char* pMac, int res, const char* pName);
 //设备断开回调
 typedef void __stdcall onDeviceDisConnect(rbt_win_context* context, const char* pMac);
 //按键回调
@@ -50,9 +50,9 @@ typedef void __stdcall onError(rbt_win_context* context, const char* pmac, int c
 //清楚画布回调
 typedef void __stdcall onClearCanvas(rbt_win_context* context, const char* pmac);
 //优化笔记回调
-typedef void __stdcall onOptimizeData(rbt_win_context* ctx, const char* pMac, ushort us, ushort ux, ushort uy, float width,float speed);
+typedef void __stdcall onOptimizeData(rbt_win_context* ctx, const char* pMac, ushort us, ushort ux, ushort uy, float width, float speed);
 
-void rbt_win_set_accept_cb( onAccept* arg);
+void rbt_win_set_accept_cb(onAccept* arg);
 void rbt_win_set_errorpacket_cb(onErrorPacket* arg);
 void rbt_win_set_origindata_cb(onOriginData* arg);
 void rbt_win_set_devicemac_cb(onDeviceMac* arg);
@@ -68,13 +68,13 @@ void rbt_win_set_optimizedata_cb(onOptimizeData *arg);
 
 typedef struct _Init_Param
 {
-	char* pIp; //本机ip，默认为NULL
+	char ip[32]; //本机ip，默认为空
 	int port;	//监听端口，6001
 	int listenCount; //最大连接数 默认60
 	bool open;  //是否打开模组， 默认打开
-	rbt_win_context* ctx; //上下文指针
 	bool optimize;	//是否输出优化笔记，默认关闭
-	_Init_Param() :pIp(nullptr), port(6001), listenCount(60), open(true), optimize(false), ctx(nullptr) {}
+	rbt_win_context* ctx; //上下文指针
+	_Init_Param() :ip(""), port(6001), listenCount(60), open(true), optimize(false), ctx(nullptr) {}
 }Init_Param;
 
 #endif
